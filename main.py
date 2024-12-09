99902752cab2ffc595ba5c63e05f5d6f2cf22fce
RudraSalunkhe <rudrasalunkhe61@gmail.com>
2024-12-09 18:32:53 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+a="rudra"
+b="hello world"
+if(a is b):
+    print("true")
+if(a is not b):
+    print("false")
+a=[1,2,3,4,5]
+b=[1,2,3,4,5]
+c=a
+print(a is b)
+print(a is c)
+print(a is not c)
+print(a is not b)
